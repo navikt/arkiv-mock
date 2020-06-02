5f9078910bd1c9d8e4a3e305a315861ffd3f22f4
nils-arne <50109859+nils-arne@users.noreply.github.com>
2020-06-02 11:01:29 +0200

    Lagt inn inmemory database og initialisering av denne

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS joark;
+
+CREATE TABLE joark (
+                       id VARCHAR(255) PRIMARY KEY,
+                       message VARCHAR(255),
+                       name VARCHAR(255)
+);
---
+++ src/main/resources/db/migration/V1.0__initial_tables.sql
@@ -0,0 +1,5 @@
+CREATE TABLE joark (
+       id VARCHAR(255) PRIMARY KEY,
+       message VARCHAR(255),
+       name VARCHAR(255)
+);
