a582844fe858b3dc40abb6456db9df05427ed71d
nils-arne <50109859+nils-arne@users.noreply.github.com>
2021-07-29 16:38:51 +0200

    Fjernet RestConfig (erstattet av tokensupport protected). Satt alle restmetoder untatt opprettjournalpost til Unprotected.

--- src/main/kotlin/no/nav/soknad/arkivering/arkivmock/rest/ArkivRestInterface.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/arkivmock/rest/ArkivRestInterface.kt
@@ -7,13 +7,14 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import no.nav.security.token.support.core.api.Protected
+import no.nav.security.token.support.core.api.Unprotected
 
 @RestController
 @RequestMapping("/rest/journalpostapi/v1")
+@Protected
 class ArkivRestInterface(private val arkivMockService: ArkivMockService) {
 	private val logger = LoggerFactory.getLogger(javaClass)
 
-	@Protected
 	@PostMapping(value = ["/journalpost"])
 	fun receiveMessage(@RequestBody arkivData: ArkivData): ResponseEntity<String> {
 		logger.info("Received message with id '${arkivData.eksternReferanseId}'")
@@ -23,6 +24,7 @@ class ArkivRestInterface(private val arkivMockService: ArkivMockService) {
 	}
 
 	@DeleteMapping(value = ["/reset"])
+	@Unprotected
 	fun reset() {
 		logger.info("Will reset database")
 
