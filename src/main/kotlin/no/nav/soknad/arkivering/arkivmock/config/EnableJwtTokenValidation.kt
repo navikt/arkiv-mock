ac5a262d9fa604818b7f99d2a4ab286c2fe6f41c
nils-arne <50109859+nils-arne@users.noreply.github.com>
2021-07-29 15:27:37 +0200

    Lagt til jwtconfig og endret tilbake bruk av ekstern database ved deploy til dev

---
+++ src/main/kotlin/no/nav/soknad/arkivering/arkivmock/config/EnableJwtTokenValidation.kt
@@ -0,0 +1,16 @@
+package no.nav.soknad.arkivering.arkivmock.config
+
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@EnableJwtTokenValidation(ignore = [
+	"org.springframework",
+	"no.nav.soknad.arkivering.arkivmock.rest.HealthCheck",
+	"io.swagger",
+	"org.springdoc",
+	"org.webjars.swagger-ui"
+])
+@Profile("dev")
+@Configuration
+class JwtTokenValidationConfig
