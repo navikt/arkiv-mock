1eb58fa6f9be81f47221a0424d5026d758c751d8
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-12-14 09:53:31 +0100

    Bumping versions and upgrading WebSecurity to new standard

--- src/main/kotlin/no/nav/soknad/arkivering/arkivmock/config/RestConfig.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/arkivmock/config/RestConfig.kt
@@ -1,29 +1,27 @@
 package no.nav.soknad.arkivering.arkivmock.config
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig {
 
-	override fun configure(http: HttpSecurity) {
+	@Bean
+	fun filterChain(http: HttpSecurity): SecurityFilterChain {
 		http
 			.csrf().disable()
-			.authorizeRequests()
+			.authorizeHttpRequests()
 			.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-			.antMatchers(HttpMethod.POST, "/login", "/register").permitAll()
-			.antMatchers(HttpMethod.GET, "/internal").permitAll()
-			.antMatchers("/rest/journalpostapi/v1/journalpost").permitAll()
+			.requestMatchers("/rest/journalpostapi/v1/journalpost").permitAll()
 			.and()
 			.httpBasic()
 			.and()
 			.sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		return http.build()
 	}
 }
