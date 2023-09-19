58534555407d42fed60359ceb52fa633e3217bfc
nilsarne <nils-arne.ringstad@nav.no>
2023-09-19 16:45:11 +0200

    Fjernet ikke brukte imports

--- src/main/kotlin/no/nav/soknad/arkivering/arkivmock/rest/SafRestInterface.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/arkivmock/rest/SafRestInterface.kt
@@ -2,17 +2,14 @@ package no.nav.soknad.arkivering.arkivmock.rest
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.security.token.support.core.api.Protected
-import no.nav.soknad.arkivering.arkivmock.dto.ArkivData
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import com.expediagroup.graphql.client.types.GraphQLClientError
 import com.expediagroup.graphql.client.types.GraphQLClientResponse
-import com.expediagroup.graphql.client.types.GraphQLClientSourceLocation
 import no.nav.soknad.arkivering.arkivmock.config.Constants.CORRELATION_ID
 import no.nav.soknad.arkivering.arkivmock.config.Constants.HEADER_CALL_ID
 import no.nav.soknad.arkivering.arkivmock.config.Constants.NAV_CONSUMER_ID
